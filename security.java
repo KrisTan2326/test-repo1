4aca8c16a93f94a94a53cd2f2c095dac7a1a7e82
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-09-26 14:00:05 +0530

    Update security.java

--- security.java
+++ security.java
@@ -1,5 +1,7 @@
 package com.hashedin.huspark.config;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
