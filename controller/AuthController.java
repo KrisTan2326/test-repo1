49ccd6f3a8876927cf68a2633d6118384d342141
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-10-07 16:38:20 +0530

    Update AuthController.java

--- controller/AuthController.java
+++ controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.hashedin.huspark.controller;
 
+
 import com.hashedin.huspark.model.Users;
 import com.hashedin.huspark.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,3 +25,4 @@ public class AuthController {
     }
 
 }
+
